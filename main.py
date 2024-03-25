0463d6d22d5158fbad03b519002af0320127a1a3
olafinski <olafinski14@gmail.com>
2024-03-25 17:58:57 +0100

    Changed MACD function

--- main.py
+++ main.py
@@ -4,12 +4,35 @@ import numpy as np
 from import_data import *
 
 
+def calculate_ema(data, window):
+
+    ema = []
+    alpha = 2 / (window + 1)
+
+    # Initialize values for the first window as the simple moving average (SMA)
+    # because EMA requires at least window number of elements to be calculated properlu
+    ema = np.zeros(len(data))
+    sma = np.mean(data[:window])
+    for i in range(window):
+        ema[i] = sma
+
+    # Calculate the EMA for the rest of the data
+    for i in range(1, len(data)):
+        ema[i] = alpha * data[i] + (1 - alpha) * ema[i - 1]
+
+    return ema
+
 def create_macd(data, short_window, long_window):
-    data['ShortEMA'] = data['Close'].ewm(span=short_window, adjust=False).mean()
-    data['LongEMA'] = data['Close'].ewm(span=long_window, adjust=False).mean()
+
+    data['ShortEMA'] = calculate_ema(data['Close'].tolist(), short_window)
+    data['LongEMA'] = calculate_ema(data['Close'].tolist(), long_window)
+
+    # Calculate the MACD
     data['MACD'] = data['ShortEMA'] - data['LongEMA']
 
-    data['Signal'] = data['MACD'].ewm(span=9, adjust=False).mean()
+    # Calculate the Signal line (9-period EMA of the MACD)
+    data['Signal'] = calculate_ema(data['MACD'].tolist(), 9)
+
     return data
 
 
@@ -179,7 +202,7 @@ def simulate_investment_bruteforce(data, buy_signals, sell_signals):
 
 
 if __name__ == '__main__':
-    weekly = get_daily()
+    weekly = get_weekly()
     data = weekly.copy(deep=True)
     short_window = 12
     long_window = 26
